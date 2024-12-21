0c9f917e7e8edf79f8ad70f4af8c100edb1b947a
hlaingminpaing-admin <hlaingminpaing.ygn@gmail.com>
2024-12-21 19:53:12 +0630

    update buildspec

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Welcome to ECS with CodeBuild Blue-Green Deployment!");
+  res.send("Hello, World! Welcome to ECS with CodeBuild Blue-Green Deployment! New version");
 });
 
 app.listen(port, () => {
